b0a304f6ab3568eefd1f2f2f91d5b53a49e9df51
Glitch (interesting-marble-dilophosaurus) <none>
2021-08-28 05:00:08 +0000

    🍮🚝 Checkpoint ./events/message.js:1022437/1624

--- events/message.js
+++ events/message.js
@@ -1,21 +1,22 @@
 module.exports = {
   name: "message",
   description: "client message event",
-  event: function (client, message) {
+  event: function(client, message) {
     const args = message.content
       .slice(client.prefix.length)
       .trim()
       .split(/ +/g);
     const command = args.shift().toLowerCase();
-    console.log(`${message.author.username}(${message.author.id}):  ${message.content}`)
-    let players = client.db.get('players').value()
-    let obj = {}
-    obj.name = message.author.username
-    obj.id = message.author.id
-    players.push(obj)
-    let setPlayers = [...new Set(players)]
-    client.db.set('players', setPlayers).write()
-
+    console.log(
+      `${message.author.username}(${message.author.id}):  ${message.content}`
+    );
+    let players = client.db.get("players").value();
+    let obj = {};
+    obj.name = message.author.username;
+    obj.id = message.author.id;
+    players.push(obj);
+    let setPlayers = [...new Set(players)];
+    client.db.set("players", setPlayers).write();
 
     if (client.commands.has(command)) {
       let foundCommand = client.commands.get(command);
