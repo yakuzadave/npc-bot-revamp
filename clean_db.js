543b005fac90560fcbe7f571670ac0fbdf8b2753
Glitch (npc-bot-revamp) <none>
2021-11-20 01:28:43 +0000

    🌸⏳ Checkpoint ./clean_db.js:1022437/425

--- clean_db.js
+++ clean_db.js
@@ -4,13 +4,21 @@ const low = require("lowdb");
 const db = low(adapter);
 
 
-let members = db.get('members').value()
+let players = db.get('players')
 
 
-let unique_members = [...new Set(members)];
+let unique_players = [...new Set(players.map(player => player['name']).value())];
+
+console.log(`There are ${unique_players.length} unique members in the DB`)
+
+let res = []
+
+let unique_player_obj = unique_players.map(unique_player => db.get)
 
 
-db.set('members', unique_members).write()
+
+
+//db.set('members', unique_members).write()
 
 
 
