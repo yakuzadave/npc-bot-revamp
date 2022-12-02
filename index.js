a478a25b2c660c18e0409c8dc6e268a6aabe8275
Glitch (npc-bot-revamp) <none>
2022-12-02 23:24:00 +0000

    🌴🌶 Checkpoint ./index.js:1022437/9873

--- index.js
+++ index.js
@@ -1,81 +1,60 @@
 // Add in Deps for the bot
 
 // Configs
-import dotenv from "dotenv"
-import fs from 'fs'
-import express from "express"
-import axios from 'axios'
-const uuid = import('uuid')
-import { Client, GatewayIntentBits, Collection, Events } from 'discord.js'
-import path from 'path'
-import { join, dirname } from 'node:path'
-import { fileURLToPath } from 'node:url'
-import { Low } from 'lowdb'
-import { JSONFile } from 'lowdb/node'
+import dotenv from "dotenv";
+import fs from "fs";
+import express from "express";
+import axios from "axios";
+const uuid = import("uuid");
+import { Client, GatewayIntentBits, Collection, Events } from "discord.js";
+import path from "path";
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import { Low } from "lowdb";
+import { JSONFile } from "lowdb/node";
 
-
-const ready =  import('./events/ready.js')
-const {Logger} = import("./modules/Logger.js")
+const ready = import("./events/ready.js");
+const { Logger } = import("./modules/Logger.js");
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const file = join(__dirname, 'db.json')
-const adapter = new JSONFile(file)
-const db = new Low(adapter)
+const file = join(__dirname, "db.json");
+const adapter = new JSONFile(file);
+const db = new Low(adapter);
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-
 // import Environment Variables
 dotenv.config();
-const token = process.env.TOKEN
+const token = process.env.TOKEN;
 
+client.commands = new Collection();
 
-
-
-
-
-// db defaults
-db.data || {
-  commands: [],
-  events: [],
-  skills: {},
-  players: [],
-  mobs: [],
-  objects: {},
-  weapons: {},
-  armor: {},
-  ships: {},
-  resources: {},
-  time: 0,
-  locations: {},
-  count: 0
-}
-
-
-
-client.commands = new Collection()
-
-let login = Promise.resolve(client.login(token)).then(res => {
-  console.log("Logged into Discord")
-  client.db = db
+let login = Promise.resolve(client.login(token)).then(async(res) => {
+  console.log("Logged into Discord");
+  await db.read()
   client.events = new Collection();
-  console.log(db)
-  client.on("ready", ()=> {
-    console.log("Discord Client is now ready")
-  })
-  
-  
-})
+  if (db.data == null) {
+    // db defaults
+    db.data ={
+      commands: [],
+      events: [],
+      skills: {},
+      players: [],
+      mobs: [],
+      objects: {},
+      weapons: {},
+      armor: {},
+      ships: {},
+      resources: {},
+      time: 0,
+      locations: {},
+      count: 0,
+    };
+  }
+  console.log(db);
+  client.db = db;
+  client.on("ready", () => {
+    console.log("Discord Client is now ready");
+  });
+});
 
-
-
-export default client
-
-
-
-
-
-
-
-
-
-
+export default client;
