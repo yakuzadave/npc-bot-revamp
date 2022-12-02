ea6ff3a20fddfe0401d9c35f8e0b0963c987adcb
Glitch (npc-bot-revamp) <none>
2022-12-02 20:01:38 +0000

    💷🌋 Checkpoint ./package.json:1022437/3292 ./index.js:1022437/711

--- index.js
+++ index.js
@@ -1,32 +1,51 @@
 // Add in Deps for the bot
 
 // Configs
-require("dotenv").config();
-const fs = require("fs");
+import dotenv from "dotenv"
+import fs from 'fs'
+import express from "express"
+import axios from 'axios'
+import {} from 'uuid'
+
+import path from 'path'
+import { join, dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { Low } from 'lowdb'
+import { JSONFile } from 'lowdb/node'
+
+
+//import {router} from './routes/router.js'
+dotenv.config();
+
 
 
 //Express Server and Routes 
-const express = require("express");
+// const express = require("express");
 const app = express();
-const {router} = require('./routes/router.js')
+// const {router} = require('./routes/router.js')
 
 
 
 // Utility
-const axios = require("axios");
+// const axios = require("axios");
 //const dialogflow = require('@google-cloud/dialogflow');
-const uuid = require('uuid');
-const path = require('path')
+// const uuid = require('uuid');
+// const path = require('path')
 
 
 
 
 
 //Load lowdb and set defaults  (this is a working exampe and may need to change later)
-const Filesync = require("lowdb/adapters/FileSync");
-const adapter = new Filesync("db.json");
-const low = require("lowdb");
-const db = low(adapter);
+// const Filesync = require("lowdb/adapters/FileSync");
+// const adapter = new Filesync("db.json");
+// const low = require("lowdb");
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const file = join(__dirname, 'db.json')
+const adapter = new JSONFile(file)
+const db = new Low(adapter)
+// const db = low(adapter);
 db.defaults({
   commands: [],
   events: [],
