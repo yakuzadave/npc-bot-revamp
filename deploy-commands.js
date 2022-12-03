3bc8bfedc4794cfc21aa08eff7b16020ad648e56
Glitch (npc-bot-revamp) <none>
2022-12-03 22:31:54 +0000

    👨🍔 Checkpoint ./deploy-commands.js:1022437/409

--- deploy-commands.js
+++ deploy-commands.js
@@ -10,30 +10,32 @@ let token = process.env.TOKEN
 let clientId = process.env.CLIENT_ID
 
 
-const commands = [];
+//const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-commandFiles.forEach( async (file) => {
+let commands = commandFiles.map( async (file) => {
 	const command = await import(`./commands/${file}`);
 	//commands.push(command.data.toJSON());
   // console.log(JSON.stringify(command))
-  commands.push(JSON.stringify(command))
+  return JSON.stringify(command)
 })
 
 const rest = new REST({ version: '10'})
 console.log(rest)
-rest.setToken(token)
+//rest.setToken(token)
 
-(async () =>{
+let deploy_tokens = async (commands, rest, Routes, token) => {
   try {
+    rest.setToken(token)
     console.log(`Starting refresh of ${commands.length} commands`)
     const data = await rest.put(
       Routes.applicationGuildCommands(clientId, guildId),
       {body: commands},
     );
     console.log(`Successfully reloaded ${data.length} commands`)
-    
   } catch (error) {
     console.error(error);
   }
-})
+}
+
+deploy_tokens(commands, rest, Routes, token)
