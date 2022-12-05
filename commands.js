cb852d05c85e553aa8a6985503effac04a1c9804
Glitch (npc-bot-revamp) <none>
2022-12-05 21:30:09 +0000

    🏮🍗 Checkpoint ./loader.js:1022437/123 ./commands.js:1022437/348

---
+++ commands.js
@@ -0,0 +1,11 @@
+import { SlashCommandBuilder, CommandInteraction, Collection } from 'discord.js'
+
+
+export const command = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute (interaction) {
+		await interaction.reply('Pong!');
+	},
+};
